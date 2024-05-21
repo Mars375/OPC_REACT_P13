d47c1e9051fd653d0789ea340b1d9d8dbb0e9d07
Mars375 <loic_2610@hotmail.fr>
2024-05-21 14:21:07 +0200

    Réorganisation des dossiers et structure du projet

---
+++ Front/src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage: React.FC = () => {
+	return (
+		<div className='container mx-auto p-4'>
+			<h1 className='text-2xl font-bold'>Login Page</h1>
+			<p>Please log in to access your account.</p>
+		</div>
+	);
+};
+
+export default LoginPage;
